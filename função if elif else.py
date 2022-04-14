822c0547ad49f22ae49eca0bd4485667ef30bdb7
Felipe Romero <felipelipos@hotmail.com>
2022-04-14 15:31:00 -0300

    Delete função if elif else.py

--- função if elif else.py
+++
@@ -1,9 +0,0 @@
-medida = int(input("insira o valor da Medida: "))
-
-if medida >200:
-    print ("reparar peça, esteira amerela")
-elif medida ==200:
-    print ("peça aprovada, esteira verde")
-else:
-    print ("peça rejeitada, esteira vermelha")
-    
